5bf0eb0cb0665cdb5dc0a057228c872d73e14465
Rafał Borowski <moderator9781@gmail.com>
2023-08-29 21:27:57 +0200

    Add authorisation for admin page + create 403 error template

--- routes/demo.js
+++ routes/demo.js
@@ -108,17 +108,30 @@ router.post("/login", async function (req, res) {
   req.session.user = { id: existingUser._id, email: existingUser.email };
   req.session.isAuthenticated = true;
   req.session.save(function () {
-    res.redirect("/admin");
+    res.redirect("/profile");
   }); // in case we are dealing with data in a database
 });
 
-//lesson 479
-router.get("/admin", function (req, res) {
+router.get("/admin", async function (req, res) {
   if (!req.session.isAuthenticated) {
     return res.status(401).render("401");
   }
 
+  const user = db.getDb().collection("users").findOne({_id: req.session.user.id})
+  
+  if (!user || !user.isAdmin){
+    return res.status(403).render("403");
+  }
+
   res.render("admin");
+});
+
+router.get("/profile", function (req, res) {
+  if (!req.session.isAuthenticated) {
+    return res.status(401).render("401");
+  }
+
+  res.render("profile");
 });
 
 router.post("/logout", function (req, res) {
